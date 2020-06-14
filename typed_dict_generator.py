ef07dd7a3ddb148328e9c99bf0d0c2d4f86e7b62
Emerentius <emerentius@arcor.de>
2020-06-14 19:55:07 +0200

    generate code for typed dicts

--- typed_dict_generator.py
+++ typed_dict_generator.py
@@ -1,13 +1,29 @@
 from typing import Dict, Any, Union, List, Optional
 from mypy_extensions import TypedDict
+import re
+from dataclasses import dataclass
 
 
-def generate_typed_dict(name: str, dict: Dict[str, Any]) -> TypedDict:
-    types_of_keys = {key: get_type(key, val) for key, val in dict.items()}
-    return TypedDict(name, types_of_keys)
+@dataclass
+class TypedDictPrinter:
+    """When printed, returns the Python code to generate a TypedDict."""
+
+    name: str
+    dict_: Dict[str, Any]
+
+    def __str__(self) -> str:
+        pairs = (f'"{key}": {value}' for key, value in self.dict_.items())
+        dict_str = "{ " + ", ".join(pairs) + " }"
+
+        # types of built-ins convert to str like this: <class 'classname'>
+        # this substitution extracts 'classname' from that
+        dict_str = re.sub(r"<class '(\w+)'>", lambda match: match.group(1), dict_str)
+        dict_str = dict_str.replace("typing.", "")
+
+        return f'TypedDict("{self.name.title()}", {dict_str})'
 
 
-def get_type(key: str, value: Any) -> Union[None, type, List[type], TypedDict]:
+def get_type(key: str, value: Any) -> Union[None, type, List[type], TypedDictPrinter]:
     type_ = value.__class__
     if value is None:
         # strictly speaking, None is of type NoneType
@@ -24,9 +40,14 @@ def get_type(key: str, value: Any) -> Union[None, type, List[type], TypedDict]:
         return List[Union[tuple(all_types)]]
     if type_ == dict:
         assert key is not None
-        return generate_typed_dict(key, value)
+        types_of_keys = {key: get_type(key, val) for key, val in value.items()}
+        return TypedDictPrinter(key, types_of_keys)
 
     raise ValueError("type not supported")
+
+
+def generate_typed_dict_code(name: str, dictionary: Dict) -> str:
+    return str(get_type(name, dictionary))
 
 
 some_dict = {
@@ -36,8 +57,5 @@ some_dict = {
     "dict": {"some_key": "blub"},
     "some_list": ["foo", "bar"],
 }
-Foo = generate_typed_dict("Foo", some_dict)
-
-foo = Foo(some_dict)
-print(repr(Foo))
-print(foo["name"])
+code = generate_typed_dict_code("Foo", some_dict)
+print(code)
