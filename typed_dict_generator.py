4233f2e4a8a90236bbcf76586d9c34f4752da167
Emerentius <emerentius@arcor.de>
2020-06-14 23:32:16 +0200

    extraction of nested typed dicts
    so they can later be defined as their own types
    and referenced in the larger typed dicts

--- typed_dict_generator.py
+++ typed_dict_generator.py
@@ -16,6 +16,13 @@ import re
 import itertools
 from dataclasses import dataclass
 
+# The KeyPath is the sequence of key accesses required to get to a certain
+# value, represented by `key1.key2.key3` etc.
+# All TypedDicts under the same path are likely to be of the same type, so we need
+# to be able to clearly identify them so that the generated dicts for multiple
+# examples can be unified.
+KeyPath = NewType("KeyPath", str)
+
 
 @dataclass
 class TypedDictPrinter:
@@ -76,8 +83,54 @@ def get_type(key: str, value: Any) -> Union[None, type, List[type], TypedDictPri
     raise ValueError("type not supported")
 
 
-def generate_typed_dict_code(name: str, dictionary: Dict) -> str:
+def generate_typed_dict_code(name: str, dictionary: Dict[str, Any]) -> str:
     return str(get_type(name, dictionary))
+
+
+def find_all_typed_dicts(
+    name: str, dict_: Dict[str, Any]
+) -> Iterator[Tuple[KeyPath, TypedDictPrinter]]:
+    toplevel_dict = get_type(name, dict_)
+    assert isinstance(toplevel_dict, TypedDictPrinter)
+    yield from _find_all_typed_dicts(KeyPath(name), toplevel_dict)
+
+
+def _find_all_typed_dicts(
+    path: KeyPath, value: Any
+) -> Iterator[Tuple[KeyPath, TypedDictPrinter]]:
+    """Recursive helper fn for a depth-first search through the typed dictionary"""
+    if isinstance(value, str):
+        return
+
+    if isinstance(value, TypedDictPrinter):
+        yield path, value
+        for key, val in value.dict_.items():
+            yield from _find_all_typed_dicts(KeyPath(f"{path}.{key}"), val)
+        return
+
+    try:
+        origin = value.__origin__
+    except AttributeError:
+        return
+
+    if origin == list:
+        it = _find_all_typed_dicts(path, value.__args__)
+    elif origin == Union:
+        it = iter(value.__args__)
+    else:
+        raise Exception(f"Unsupported type: {value}")
+
+    for val in it:
+        yield from _find_all_typed_dicts(path, val)
+
+
+def accumulate_typed_dicts(
+    name: str, dict_: Dict
+) -> Dict[KeyPath, List[TypedDictPrinter]]:
+    accumulated_dicts: Dict = {}
+    for path, typed_dict in find_all_typed_dicts(KeyPath(name), dict_):
+        accumulated_dicts.setdefault(path, []).append(typed_dict)
+    return accumulated_dicts
 
 
 some_dict = {
@@ -86,6 +139,12 @@ some_dict = {
     "floaty": 1.0,
     "dict": {"some_key": "blub"},
     "some_list": ["foo", "bar"],
+    "heterogenous_list": ["string", 1.0],
+    # "heterogenous_list": ["string", 1.0, {"quux": "fox"}],
 }
-code = generate_typed_dict_code("Foo", some_dict)
-print(code)
+
+for val in find_all_typed_dicts("Foo", some_dict):
+    print(val)
+
+print("\naccumulated:")
+print(accumulate_typed_dicts("Foo", some_dict))
